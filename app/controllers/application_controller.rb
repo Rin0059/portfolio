1f879c3157747c42d47d24cb55ee39239e46883b
Rin0059 <fandorin0059@gmail.com>
2021-07-02 08:23:50 +0000

    Add admins and users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+  private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sing_up,keys[:name, :email, :encrypted_password])
+    end
+
+    def after_sign_in_path_for(resource_or_scope)
+      if resource_or_scope.is_a?(Admin)
+        admin_root_path
+      else
+        root_path
+      end
+    end
+
+    def after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :customer
+        root_path
+      elsif resource_or_scope == :admin
+        new_admin_session_path
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+    root to: 'home#index'
 end
