db0c9fdb80f1b909d6d68d4ae4dd95696cccca54
Rin0059 <fandorin0059@gmail.com>
2021-07-29 11:48:06 +0000

    Add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: 'fandorin0059@gmail.com',
+  password: 'rin0059',
+  password_confirmation: 'rin0059'
+)
